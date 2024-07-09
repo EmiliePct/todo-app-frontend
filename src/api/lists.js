828f0a8b386e5798343b002a687f7d9694c06273
Emilie <emilie.pechot@gmail.com>
2024-07-10 00:12:34 +0200

    creation et suppression d'une liste

--- src/api/lists.js
+++ src/api/lists.js
@@ -1,6 +1,6 @@
+// Get toutes les listes d'un user
 export const getLists = async (userId, accessToken) => {
     try {
-        console.log('rentré dans API', userId, accessToken)
       const response = await fetch(`http://localhost:3000/lists/${userId}`, 
         {
             method: 'GET',
@@ -12,5 +12,72 @@ export const getLists = async (userId, accessToken) => {
       return data;
     } catch (error) {
       console.log(error);
+    }
+  };
+
+  //Get les tâches ? d'une liste d'un user, à vérifier si elle fonctionne
+  export const displayList = async (listId, accessToken) => {
+    try {
+      const response = await fetch(`http://localhost:3000/lists/${listId}`, 
+        {
+            method: 'GET',
+            mode: 'cors',
+            headers: { "Authorization": `Bearer ${accessToken}` },
+          }
+      )
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Post création d'une nouvelle liste
+  export const createList = async (
+    title, user
+  ) => {
+    try {
+      const response = await fetch(
+        `http://localhost:3000/lists`,
+        {
+          method: "POST",
+          headers: 
+          {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${user.accessToken}`,
+          },
+          body: JSON.stringify({
+            title: title,
+            userId: user.userId
+          }),
+        }
+      );
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  // DELETE suppression d'une liste et des tâches associées
+  export const deleteList = async (
+    listId, user
+  ) => {
+    try {
+      const response = await fetch(
+        `http://localhost:3000/lists/${listId}`,
+        {
+          method: "delete",
+          headers: 
+          {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${user.accessToken}`,
+          },
+        }
+      );
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error.message);
     }
   };

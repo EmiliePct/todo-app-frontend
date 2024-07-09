18a602bf5a4b0bd7d3d5463e5a887ad78af2de3d
Emilie <emilie.pechot@gmail.com>
2024-07-09 13:51:29 +0200

    ajout appbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,8 @@
-import styles from '../styles/Home.module.css';
-import { useDispatch, useSelector } from "react-redux";
+// import styles from '../styles/Home.module.css';
+// import { useDispatch, useSelector } from "react-redux";
 
-import { signOut } from "../reducers/user";
-
+// import { signOut } from "../reducers/user";
+// import { Drawer, AppBar, Icon, IconButton, MenuIcon } from '@mui/material';
 
 
 // par défaut : affiche texte qu'il n'y a rien à voir.
@@ -19,29 +19,102 @@ qui contiendra lui même :
 - right sidebar sous forme de composant 
 
 */
+import { AppBar } from '@mui/material';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import MenuIcon from '@mui/icons-material/Menu';
 
-function Home() {
-
-  const dispatch = useDispatch();
+import { useState } from "react";
 
 
-  const handleSignOut = () => {
-    dispatch(signOut());
+export default function TemporaryDrawer() {
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
   };
 
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
 
   return (
-<body>
-<p>c'est la page home</p>
-<button
-            className={styles.buttonconfirminput}
-            id="signOut"
-            onClick={() => handleSignOut()}
-          >
-            Déconnexion
-          </button>
-</body>
+    <AppBar>
+      <header>      <Button onClick={toggleDrawer(true)}><MenuIcon color="secondary"></MenuIcon></Button><span>To-do App</span></header>
+
+      <Drawer open={open} onClose={toggleDrawer(false)}>
+        {DrawerList}
+      </Drawer>
+    </AppBar>
   );
 }
 
-export default Home;
+// function Home() {
+
+//   const dispatch = useDispatch();
+
+
+//   const handleSignOut = () => {
+//     dispatch(signOut());
+//   };
+
+
+//   return (
+//     <body>
+//       <p>c'est la page home</p>
+//       <AppBar>
+//         {/* <IconButton color="secondary" aria-label="open menu">
+//           <MenuIcon />
+//         </IconButton> */}
+//         <p>c'est l'AppBar</p>
+//       </AppBar>
+//       <Drawer>
+//         <p>c'est le Drawer</p>
+//       </Drawer>
+//       <button
+//         className={styles.buttonconfirminput}
+//         id="signOut"
+//         onClick={() => handleSignOut()}
+//       >
+//         Déconnexion
+//       </button>
+//     </body>
+//   );
+// }
+
+// export default Home;
